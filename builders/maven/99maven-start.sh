0c43670ed119674056c2a110f193024322462316
TecHunter <techunter@users.noreply.github.com>
2021-03-31 19:13:25 +0200

    fix wrong bin path

--- builders/maven/99maven-start.sh
+++ builders/maven/99maven-start.sh
@@ -3,10 +3,10 @@
 export JENKINS_HOME=/home/jenkins
 export BIN_DIR=/home/jenkins/bin
 
-if [ -L ${BIN_DIR}/bin/mvn ] && [ -e ${BIN_DIR}/bin/mvn ] ; then
+if [ -L ${BIN_DIR}/mvn ] && [ -e ${BIN_DIR}/mvn ] ; then
    echo "Maven linked"
 else
-   rm -f ${BIN_DIR}/bin/mvn || echo 0
-   ln -s ${JENKINS_HOME}/maven/bin/mvn ${BIN_DIR}/bin/mvn
+   rm -f ${BIN_DIR}/mvn || echo 0
+   ln -s ${JENKINS_HOME}/maven/bin/mvn ${BIN_DIR}/mvn
 fi
 
